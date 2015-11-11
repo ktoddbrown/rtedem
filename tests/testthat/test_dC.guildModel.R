05d1cb3b4ccd52028026c16606b87f2a6b8fcf09
ktoddbrown <ktoddbrown@gmail.com>
2015-11-10 22:31:52 -0800

    reproduce first order linear model with general guild model

--- tests/testthat/test_dC.guildModel.R
+++ tests/testthat/test_dC.guildModel.R
@@ -13,26 +13,16 @@ context("dC.guildModel")
 
 test_that('dC.guildModel produces expected errors',{})
 
-test_that('dC.guildModel', {
+test_that('dC.guildModel reproduces a simple first order model', {
   reactionNetwork <- data.frame(from=c('C1', 'C1', 'C2', 'C2'), 
                                 to=c(NA, 'C2', NA, 'C1'), 
-                                reaction=c('k1*(1-trans2)*C1', 'k1*trans2*C1', 'k2*(1-trans3)*C2', 'k2*trans3*C2'),
+                                reaction=c('1/tau1*(1-trans2)*C1', '1/tau1*trans2*C1', 
+                                           '1/tau2*(1-trans3)*C2', '1/tau2*trans3*C2'),
                                 type=c('decay', 'transfer', 'decay', 'transfer'),
                                 stringsAsFactors=FALSE)
   y <- c(C1=1, C2=3)
-  dy <- y
-  parms <- list(k1=0.1, k2=0.01, trans2=0.5, trans3=0.1)
-  attach(c(as.list(y), parms))
+  parms <- unlist(list(tau1=10, tau2=100, trans2=0.5, trans3=0.1))
   
-  temp <- ddply(reactionNetwork, c('from', 'to', 'reaction'), summarize, value = eval(parse(text = as.character(reaction))))
-  detach(c(as.list(y), parms))
-  dC <- data.frame()
-  for(idStr in unique(c(temp$to, temp$from))){
-    dC <- rbind.fill(dC, data.frame(id=idStr, 
-                                    value=sum(temp$value[temp$to %in% idStr]) -
-                                          sum(temp$value[temp$from %in% idStr])))
-  }
-  dy <- dC$value[!is.na(dC$id)]
-  names(dy) <- dC$id[!is.na(dC$id)]
-  dy <- dy[names(y)]
+  expect_equal(dC.firstOrderModel(t=0, y=y, parms=parms, transStr='trans'),
+                  dC.guildModel(t=0, y=y, parms=parms, reactionNetwork=reactionNetwork))
 })
