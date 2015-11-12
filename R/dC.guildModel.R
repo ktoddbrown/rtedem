10bae45b7d7501245d1302faeb04ebdc2efab319
ktoddbrown <ktoddbrown@gmail.com>
2015-11-11 17:20:23 -0800

    update file name

--- R/dC.guildModel.R
+++ R/dC.guildModel.R
@@ -1,3 +1,14 @@
+#' Generalized decomposition model
+#'
+#' @param t numeric for timepoint
+#' @param y named array pools
+#' @param parms named array of parameters 
+#' @param reactionNetwork data.frame with from, to identifying the pools named in y which the flux is leaving and entering (NA is valid to represent transfers outside the system), reaction is a string which will be evaluated in the context of the named pools and parameters passed above.
+#' @param verbose boolean flag for useful debugging output
+#'
+#' @return a named array with the change in pools
+#' @export
+#'
 dC.guildModel <- function(t, y=c(C1=1, C2=3), 
                           parms=unlist(list(k1=0.1, k2=0.01, trans2=0.5, trans3=0.1)), 
                           reactionNetwork = data.frame(from=c('C1', 'C1', 'C2', 'C2'), 
@@ -14,7 +25,7 @@ dC.guildModel <- function(t, y=c(C1=1, C2=3),
   if(verbose)cat('declared vars:[', sort(unique(c(names(y), names(parms)))), ']\n')
   assert_that(identical(usedVars, sort(unique(c(names(y), names(parms))))))
   
-  if(verbose) {cat('evalEnv:\n'); print(as.list(c(y, parms)))}
+  if(verbose) {cat('evalEnv:\n'); print(as.list(c(y, parms, t=t)))}
   temp <- ddply(reactionNetwork, c('from', 'to', 'reaction'), function(xx, evalEnv=as.list(c(y, parms))){ data.frame(value = eval(parse(text = xx$reaction), envir=evalEnv))})
   
   if(verbose) {cat('temp:\n'); print(temp)}
